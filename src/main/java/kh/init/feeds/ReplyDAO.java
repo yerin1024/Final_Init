e9ffdd6cf2b1f00ae88598501a0510de186e3985
kobe <a@b.c>
2020-01-09 11:48:20 +0900

    mapper deleteReply,deleteFeed #{col},#{val} modify

--- src/main/java/kh/init/feeds/ReplyDAO.java
+++ src/main/java/kh/init/feeds/ReplyDAO.java
@@ -1,6 +1,8 @@
 package kh.init.feeds;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +17,13 @@ public class ReplyDAO {
 	public int registerReply(FeedDTO dto)throws Exception{
 		return jdbc.insert("Feed.registerReply", dto);
 	}
-	public int deleteReply(ReplyDTO dto)throws Exception{
-		return jdbc.delete("Feed.deleteReply", dto);
+	public int deleteReply(String col,int val)throws Exception{
+		Map<String,Object> param = new HashMap<>();
+		param.put("col", col);
+		param.put("val", val);
+		return jdbc.delete("Feed.deleteReply",param);
 	}
 	public List<ReplyDTO> viewReply(int feed_seq)throws Exception{
 		return jdbc.selectList("Feed.viewReply",feed_seq);
-	}
-	public int deleteFeedAndReply(ReplyDTO dto)throws Exception{
-		return jdbc.delete("Feed.deleteFeedAndReply", dto);
 	}
 }
--- src/main/java/kh/init/home/HomeController.java
+++ src/main/java/kh/init/home/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 	@RequestMapping("/home")
 	public String home() {
 		//임시로 home에 생성하도록 했고 이후에 로그인 성공하는 순간으로 바꿔야함 
-		session.setAttribute("mediaList", new ArrayList<String>());
+//		session.setAttribute("mediaList", new ArrayList<String>());
 		return "home";
 	}
 	
