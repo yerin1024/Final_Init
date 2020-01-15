eb3691b6ba591d6065ba8979ffbf0bb000e9d81e
chloe <chloe@gmail.com>
2020-01-15 22:47:51 +0900

    MemberService javamail api import edited.

--- src/main/java/kh/init/members/MemberService.java
+++ src/main/java/kh/init/members/MemberService.java
@@ -1,10 +1,11 @@
 package kh.init.members;
 
 import java.io.File;
-import java.net.PasswordAuthentication;
 import java.util.Properties;
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -14,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.google.inject.spi.Message;
 
 import kh.init.configuration.Configuration;
 import kh.init.configuration.Utils;
