079ed7a379c840407f30ba45421d3b8c9770fed2
chloe <chloe@gmail.com>
2020-01-17 09:27:56 +0900

    login session test code added into memberControll, FeedController

--- src/main/java/kh/init/members/MemberController.java
+++ src/main/java/kh/init/members/MemberController.java
@@ -29,6 +29,10 @@ public class MemberController {
 		}
 		if(service.isLoginOk(email, pw) > 0) { // 로그인 허가
 			session.setAttribute("loginInfo", service.getMemberDTO(email)); // 세션 로그인정보 담기
+			//로그인 세션 테스트 코드 시작
+			String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+			System.out.println("로그인 세션 값 확인 : " + email1);
+			//로그인 세션 테스트 코드 끝
 			return "redirect:/feed/getFriendFeed";
 		}else {
 			return "main";
@@ -56,6 +60,10 @@ public class MemberController {
 	// 로그아웃 세션 삭제
 	@RequestMapping("/logout.do")
 	public String toLogout() {
+		//로그인 세션 테스트 코드 시작
+		String email = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email);
+		//로그인 세션 테스트 코드 끝
 		System.out.println("로그아웃 > " + session.getAttribute("loginInfo").toString() + " 세션 삭제");
 		session.removeAttribute("loginInfo");
 		return "main";
@@ -67,6 +75,10 @@ public class MemberController {
 	public String toFindPwProc(String email) {
 		System.out.println("사용자 이메일  : " + email);
 		JsonObject obj = new JsonObject();
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		
 		if(service.findPw(email) == "invalid") {
 			obj.addProperty("result", "invalid");
@@ -79,6 +91,10 @@ public class MemberController {
 	@RequestMapping("/goMyInfo")  //내 정보 (편집) 가기
 	public String goMyInfo(String email, Model model) {
 		System.out.println("개인 정보 CON 도착.");
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		MemberDTO mDto = (MemberDTO)session.getAttribute("loginInfo");
 		try {			
 			MemberDTO dto = service.getMyPageService(mDto.getEmail());
@@ -107,6 +123,10 @@ public class MemberController {
 	@RequestMapping("/goMyProfile") //내 프로필(편집) 가기
 	public String goMyProfile(String email, Model model) {
 		System.out.println("개인 프로필 수정 CON 도착.");
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		try {
 			MemberDTO mDto = (MemberDTO)session.getAttribute("loginInfo");
 			MemberDTO dto = service.getMyPageService(mDto.getEmail());
@@ -126,6 +146,10 @@ public class MemberController {
 	@RequestMapping("/withdrawMem") //회원 탈퇴 하기
 	public String getout() {
 		System.out.println("회원 탈퇴 CON 도착.");
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		try {
 			MemberDTO mDto = (MemberDTO)session.getAttribute("loginInfo");
 			int result = service.withdrawMemService(mDto.getEmail());
@@ -152,6 +176,10 @@ public class MemberController {
 	@RequestMapping("/changeMyInfo") //회원 정보 수정하기
 	public String changeInfo(MemberDTO dto) {
 		System.out.println("회원 정보 수정 CON 도착.");
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		try {
 			int result = 0;
 			MemberDTO mDto = (MemberDTO)session.getAttribute("loginInfo");
@@ -181,6 +209,10 @@ public class MemberController {
 	@RequestMapping("/changeProfile") //프로필 바꿔버리기
 	public String changeMyProfile(MemberDTO dto, MultipartFile profileImg) {
 		System.out.println("회원 정보 수정 CON 도착.");
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		String path = session.getServletContext().getRealPath("files");
 		MemberDTO mDto = (MemberDTO)session.getAttribute("loginInfo");
 		int result = 0;
@@ -214,6 +246,10 @@ public class MemberController {
 	public String identifyMemPw(String pw) {
 		System.out.println("현재 비밀번호 확인 CON 도착"); 
 		System.out.println("현재 적은 비번은 "+pw);
+		//로그인 세션 테스트 코드 시작
+		String email1 = ((MemberDTO)session.getAttribute("loginInfo")).getEmail();
+		System.out.println("로그인 세션 값 확인 : " + email1);
+		//로그인 세션 테스트 코드 끝
 		try {
 			MemberDTO mDto = (MemberDTO)session.getAttribute("loginInfo");
 			MemberDTO dto = service.identifyMemPwService(mDto.getEmail());
